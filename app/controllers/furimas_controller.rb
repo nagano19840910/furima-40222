5e7aba2cdb1f2a4aecb6f91c3bedf52c8f3526c6
nagano19840910 <pacino0167@gmail.com>
2024-01-17 16:24:25 +0900

    新規登録機能

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -2,4 +2,20 @@ class FurimasController < ApplicationController
   def index
     @furimas = Furima.all
   end
+
+  def new
+    @furimas = Furima.new
+  end
+
+  def create
+    Furima.create(furima_params)
+    redirect_to '/'
+  end
+  
+  private
+  def furima_params
+    params.require(:furima).permit(:name, :image, :text)
+  end
+
 end
+
