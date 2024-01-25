d2d99d97fd45ac8d316b82e2e81ebfe3d33253bc
nagano19840910 <pacino0167@gmail.com>
2024-01-25 17:40:01 +0900

    商品一覧機能３

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -2,7 +2,6 @@ class FurimasController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
     @furimas = Furima.order(created_at: :desc)
-    # @furimas == [ ]
   end
 
   def new
