258ba056e9094605d29e6f1c2928733e66db4d70
nagano19840910 <pacino0167@gmail.com>
2024-01-25 14:56:30 +0900

    商品一覧機能

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,10 @@
 
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", calculateFeeAndProfit);
 
 function calculateFeeAndProfit() {
   const priceInput = document.getElementById("item-price");
   const addTaxPrice = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
-
-  
   const price = parseFloat(priceInput.value);
-
   
   if (isNaN(price) || price < 300 || price > 9999999) {
     addTaxPrice.textContent = '';
@@ -17,25 +12,19 @@ function calculateFeeAndProfit() {
     return;
   }
 
-  
-  const fee = Math.floor(price * 0.1);
-
-  
+  const fee = Math.floor(price * 0.1);  
   const salesProfit = Math.floor(price - fee);
 
-  
   addTaxPrice.textContent = fee.toString();
   profit.textContent = salesProfit.toString();
+}
 
+document.addEventListener('turbo:render', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", calculateFeeAndProfit);
+});
 
-
-  
-  document.addEventListener('turbo:render', () => {
-  
-    priceInput.addEventListener("input", calculateFeeAndProfit);
-  });
-  
-  document.addEventListener('turbo:load', () => {
-  
-    priceInput.addEventListener("input", calculateFeeAndProfit);
-  });
+document.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", calculateFeeAndProfit);
+});
