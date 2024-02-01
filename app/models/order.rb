4e01e3631c0c49e167de956b7a254fab9b6c43a9
nagano19840910 <pacino0167@gmail.com>
2024-02-01 18:46:44 +0900

    終了後のデプロイエラー

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order
   include ActiveModel::Model 
-  attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id, :token, :furima_id,
+  attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :phone_number, :user_id, :token, :furima_id
   with_options presence: true do
     validates :post_code, format: {with:/\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid.Enter it as follows (e.g. 123-4567)"}
     validates :municipalities
@@ -19,5 +19,4 @@ class Order
     purchase = Purchase.create(user_id:user_id, furima_id:furima_id)
     ShippingAddress.create(post_code:post_code, prefecture_id:prefecture_id, municipalities:municipalities,street_address:street_address, building_name:building_name, phone_number:phone_number,purchase_id:purchase.id)
   end
-end  
-
+end 
