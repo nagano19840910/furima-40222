ee734b36771f6198054285f58449b1cc758cd145
nagano19840910 <pacino0167@gmail.com>
2024-02-02 14:41:52 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
-  def after_sign_in_path_for(resource)
-    root_path
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
+  # def after_sign_in_path_for(resource)
+  #   root_path
+  # end
   private
 
   def configure_permitted_parameters
