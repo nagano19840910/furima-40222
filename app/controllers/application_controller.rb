55e5dc2806c3c91d71ec2fab96fc0cd71a2a9b48
nagano19840910 <pacino0167@gmail.com>
2024-02-02 14:15:35 +0900

    終了報告４

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth,:configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     root_path
   end
